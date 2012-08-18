b191535874e135215068f5361191a89f056f67bc
Robert Giseburt <giseburt@gmail.com>
2012-08-17 22:54:49 -0500

    Refactored Pin<>, added Port<> and PinHolder<>
    PinHolder<> works better than I expected. :-)

--- Arduino Library/Motate/MotatePins.h
+++ Arduino Library/Motate/MotatePins.h
@@ -38,33 +38,36 @@ namespace Motate {
 	};
 
 
-	namespace implementation {
-		// valid_pin_check is a partial specialization based on pinNum
-		// default is that the pin is not null
-		template <uint8_t, class T = void> 
-		struct valid_pin_check
-		{
-			bool isNull() { return false; };
-		};
-
-		// pin number -1 (255, technically, since it unsigned) is NULL
-		template <class T> 
-		struct valid_pin_check<-1, T> 
-		{
-			bool isNull() { return true; };
-		};
-	}
-	
-	struct PinBase {
-		virtual void init(const PinSetupType type) = 0;
-		virtual void set(bool value);
-		virtual uint8_t get();
+	template <unsigned char portLetter>
+	struct Port8 {
+		static const uint8_t letter = (uint8_t) portLetter;
 		
+		void setDirection(const uint8_t value, const uint8_t mask = 0xff) {
+			// stub
+		};
+		void setPins(const uint8_t value, const uint8_t mask = 0xff) {
+			// stub
+		};
+		uint8_t getPins(const uint8_t mask = 0xff) {
+			// stub
+			return 0;
+		};
 	};
+		// 
+		// struct PinBase {
+		// 	virtual void init(const PinSetupType type) = 0;
+		// 	virtual void set(bool value) = 0;
+		// 	virtual uint8_t get() = 0;
+		// 	
+		// };
+		// 
+		// typedef PinBase * pinPtr;
 	
-	template<uint8_t pinNum>
-	struct Pin : public PinBase, public implementation::valid_pin_check< pinNum > {
-		enum { number = pinNum };
+	template<int8_t pinNum>
+	struct Pin /*: public PinBase*/ {
+		static const int8_t number = pinNum;
+		static const uint8_t portLetter; // set later
+		static const uint8_t mask; // set later
 				
 		Pin(const PinSetupType type = Unchanged) {
 			init(type);
@@ -72,6 +75,7 @@ namespace Motate {
 		Pin<pinNum> &operator=(const bool value) { set(value); return *this; };
 		Pin<pinNum> &operator=(const PinSetupType type) { init(type); return *this; };
 		operator bool() { return (get() != 0); };
+		
 		void init(const PinSetupType type) {
 			// stub
 		};
@@ -82,14 +86,15 @@ namespace Motate {
 			// stub
 			return 0;
 		};
+		static uint8_t maskForPort(const uint8_t otherPortLetter) {
+			return portLetter == otherPortLetter ? mask : 0x00;
+		};
+		bool isNull() { return true; };
 	};
 	
-	// Explicitly pre-define a NullPin to compare to
-	Pin<-1> NullPin;
+	typedef const int8_t pin_number;
 	
-	typedef const uint8_t pin_number;
-	
-	#define _MAKE_MOTATE_PIN(pinNum, registerLetter, registerPin)\
+	#define _MAKE_MOTATE_PIN(pinNum, registerLetter, registerChar, registerPin)\
 		template <> void Pin<pinNum>::init(const PinSetupType type) { \
 			switch (type) {\
 				case Output:\
@@ -115,146 +120,308 @@ namespace Motate {
 		template <> uint8_t Pin<pinNum>::get() { \
 			return (PIN ## registerLetter) & (1<<registerPin);\
 		};\
+		template <> const uint8_t Pin<pinNum>::portLetter = (uint8_t) registerChar;\
+		template <> const uint8_t Pin<pinNum>::mask = (1 << registerPin);\
+		template <> bool Pin<pinNum>::isNull() { return false; };\
 		typedef Pin<pinNum> Pin ## pinNum;\
-		Pin<pinNum> pin ## pinNum;
+		Pin ## pinNum pin ## pinNum;
+		
+
+
+	#define _MAKE_MOTATE_PORT8(registerLetter, registerChar)\
+	template <> void Port8<registerChar>::setDirection(const uint8_t value, const uint8_t mask) {\
+		uint8_t port_value = (DDR ## registerLetter);\
+		if (mask != 0xff) {\
+			port_value &= mask;\
+		}\
+		(DDR ## registerLetter) = port_value | value;\
+	};\
+	template <> void Port8<registerChar>::setPins(const uint8_t value, const uint8_t mask) {\
+		uint8_t port_value = (PIN ## registerLetter);\
+		if (mask != 0xff) {\
+			port_value &= mask;\
+		}\
+		(PORT ## registerLetter) = port_value | value;\
+	};\
+	template <> uint8_t Port8<registerChar>::getPins(const uint8_t mask) {\
+		return (PIN ## registerLetter) & (mask);\
+	}\
+	typedef Port8<registerChar> Port ## registerLetter;\
+	Port ## registerLetter port ## registerLetter;
+	
+	// Special NullPin and nullPin objects
+	template <> void Pin<-1>::init(const PinSetupType) {};
+	template <> void Pin<-1> ::set(bool)  {};
+	template <> uint8_t Pin<-1>::get() {return 0;};
+	template <> const uint8_t Pin<-1>::portLetter = 0;
+	template <> const uint8_t Pin<-1>::mask = 0xFF;
+	typedef Pin<-1> NullPin;
+	NullPin nullPin;
 
 #if defined(__AVR_ATmega168__) || defined(__AVR_ATmega328P__)
 
 // standard (UNO, etc)
-	_MAKE_MOTATE_PIN( 0, D, 0);
-	_MAKE_MOTATE_PIN( 1, D, 1);
-	_MAKE_MOTATE_PIN( 2, D, 2);
-	_MAKE_MOTATE_PIN( 3, D, 3);
-	_MAKE_MOTATE_PIN( 4, D, 4);
-	_MAKE_MOTATE_PIN( 5, D, 5);
-	_MAKE_MOTATE_PIN( 6, D, 6);
-	_MAKE_MOTATE_PIN( 7, D, 7);
-	_MAKE_MOTATE_PIN( 8, B, 0);
-	_MAKE_MOTATE_PIN( 9, B, 1);
-	_MAKE_MOTATE_PIN(10, B, 2);
-	_MAKE_MOTATE_PIN(11, B, 3);
-	_MAKE_MOTATE_PIN(12, B, 4);
-	_MAKE_MOTATE_PIN(13, B, 5);
-	_MAKE_MOTATE_PIN(14, C, 0);
-	_MAKE_MOTATE_PIN(15, C, 1);
-	_MAKE_MOTATE_PIN(16, C, 2);
-	_MAKE_MOTATE_PIN(17, C, 3);
-	_MAKE_MOTATE_PIN(18, C, 4);
-	_MAKE_MOTATE_PIN(19, C, 5);
+	_MAKE_MOTATE_PORT8(B, 'B');
+	_MAKE_MOTATE_PORT8(C, 'C');
+	_MAKE_MOTATE_PORT8(D, 'D');
+
+	_MAKE_MOTATE_PIN( 0, D, 'D', 0);
+	_MAKE_MOTATE_PIN( 1, D, 'D', 1);
+	_MAKE_MOTATE_PIN( 2, D, 'D', 2);
+	_MAKE_MOTATE_PIN( 3, D, 'D', 3);
+	_MAKE_MOTATE_PIN( 4, D, 'D', 4);
+	_MAKE_MOTATE_PIN( 5, D, 'D', 5);
+	_MAKE_MOTATE_PIN( 6, D, 'D', 6);
+	_MAKE_MOTATE_PIN( 7, D, 'D', 7);
+	_MAKE_MOTATE_PIN( 8, B, 'B', 0);
+	_MAKE_MOTATE_PIN( 9, B, 'B', 1);
+	_MAKE_MOTATE_PIN(10, B, 'B', 2);
+	_MAKE_MOTATE_PIN(11, B, 'B', 3);
+	_MAKE_MOTATE_PIN(12, B, 'B', 4);
+	_MAKE_MOTATE_PIN(13, B, 'B', 5);
+	_MAKE_MOTATE_PIN(14, C, 'C', 0);
+	_MAKE_MOTATE_PIN(15, C, 'C', 1);
+	_MAKE_MOTATE_PIN(16, C, 'C', 2);
+	_MAKE_MOTATE_PIN(17, C, 'C', 3);
+	_MAKE_MOTATE_PIN(18, C, 'C', 4);
+	_MAKE_MOTATE_PIN(19, C, 'C', 5);
 	
 #elif defined(__AVR_ATmega32U4__)
 
 // leonardo
-	_MAKE_MOTATE_PIN(0 , D, 2);
-	_MAKE_MOTATE_PIN(1 , D, 3);
-	_MAKE_MOTATE_PIN(2 , D, 1);
-	_MAKE_MOTATE_PIN(3 , D, 0);
-	_MAKE_MOTATE_PIN(4 , D, 4);
-	_MAKE_MOTATE_PIN(5 , C, 6);
-	_MAKE_MOTATE_PIN(6 , D, 7);
-	_MAKE_MOTATE_PIN(7 , E, 6);
-                          
-	_MAKE_MOTATE_PIN(8 , B, 4);
-	_MAKE_MOTATE_PIN(9 , B, 5);
-	_MAKE_MOTATE_PIN(10, B, 6);
-	_MAKE_MOTATE_PIN(11, B, 7);
-	_MAKE_MOTATE_PIN(12, D, 6);
-	_MAKE_MOTATE_PIN(13, C, 7);
-                          
-	_MAKE_MOTATE_PIN(14, B, 3);
-	_MAKE_MOTATE_PIN(15, B, 1);
-	_MAKE_MOTATE_PIN(16, B, 2);
-	_MAKE_MOTATE_PIN(17, B, 0);
-                          
-	_MAKE_MOTATE_PIN(18, F, 7);
-	_MAKE_MOTATE_PIN(19, F, 6);
-	_MAKE_MOTATE_PIN(20, F, 5);
-	_MAKE_MOTATE_PIN(21, F, 4);
-	_MAKE_MOTATE_PIN(22, F, 1);
-	_MAKE_MOTATE_PIN(23, F, 0);
-                          
-	_MAKE_MOTATE_PIN(24, D, 4);
-	_MAKE_MOTATE_PIN(25, D, 7);
-	_MAKE_MOTATE_PIN(26, B, 4);
-	_MAKE_MOTATE_PIN(27, B, 5);
-	_MAKE_MOTATE_PIN(28, B, 6);
-	_MAKE_MOTATE_PIN(29, D, 6);
+	_MAKE_MOTATE_PORT8(B, 'B');
+	_MAKE_MOTATE_PORT8(C, 'C');
+	_MAKE_MOTATE_PORT8(D, 'D');
+	_MAKE_MOTATE_PORT8(E, 'E');
+	_MAKE_MOTATE_PORT8(F, 'F');
+
+	_MAKE_MOTATE_PIN(0 , D, 'D', 2);
+	_MAKE_MOTATE_PIN(1 , D, 'D', 3);
+	_MAKE_MOTATE_PIN(2 , D, 'D', 1);
+	_MAKE_MOTATE_PIN(3 , D, 'D', 0);
+	_MAKE_MOTATE_PIN(4 , D, 'D', 4);
+	_MAKE_MOTATE_PIN(5 , C, 'C', 6);
+	_MAKE_MOTATE_PIN(6 , D, 'D', 7);
+	_MAKE_MOTATE_PIN(7 , E, 'E', 6);
+
+	_MAKE_MOTATE_PIN(8 , B, 'B', 4);
+	_MAKE_MOTATE_PIN(9 , B, 'B', 5);
+	_MAKE_MOTATE_PIN(10, B, 'B', 6);
+	_MAKE_MOTATE_PIN(11, B, 'B', 7);
+	_MAKE_MOTATE_PIN(12, D, 'D', 6);
+	_MAKE_MOTATE_PIN(13, C, 'C', 7);
+
+	_MAKE_MOTATE_PIN(14, B, 'B', 3);
+	_MAKE_MOTATE_PIN(15, B, 'B', 1);
+	_MAKE_MOTATE_PIN(16, B, 'B', 2);
+	_MAKE_MOTATE_PIN(17, B, 'B', 0);
+
+	_MAKE_MOTATE_PIN(18, F, 'F', 7);
+	_MAKE_MOTATE_PIN(19, F, 'F', 6);
+	_MAKE_MOTATE_PIN(20, F, 'F', 5);
+	_MAKE_MOTATE_PIN(21, F, 'F', 4);
+	_MAKE_MOTATE_PIN(22, F, 'F', 1);
+	_MAKE_MOTATE_PIN(23, F, 'F', 0);
+
+	_MAKE_MOTATE_PIN(24, D, 'D', 4);
+	_MAKE_MOTATE_PIN(25, D, 'D', 7);
+	_MAKE_MOTATE_PIN(26, B, 'B', 4);
+	_MAKE_MOTATE_PIN(27, B, 'B', 5);
+	_MAKE_MOTATE_PIN(28, B, 'B', 6);
+	_MAKE_MOTATE_PIN(29, D, 'D', 6);
 
 #elif defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
 
 // MEGA
-	_MAKE_MOTATE_PIN(0 , E, 0);
-	_MAKE_MOTATE_PIN(1 , E, 1);
-	_MAKE_MOTATE_PIN(2 , E, 4);
-	_MAKE_MOTATE_PIN(3 , E, 5);
-	_MAKE_MOTATE_PIN(4 , G, 5);
-	_MAKE_MOTATE_PIN(5 , E, 3);
-	_MAKE_MOTATE_PIN(6 , H, 3);
-	_MAKE_MOTATE_PIN(7 , H, 4);
-	_MAKE_MOTATE_PIN(8 , H, 5);
-	_MAKE_MOTATE_PIN(9 , H, 6);
-	_MAKE_MOTATE_PIN(10, B, 4);
-	_MAKE_MOTATE_PIN(11, B, 5);
-	_MAKE_MOTATE_PIN(12, B, 6);
-	_MAKE_MOTATE_PIN(13, B, 7);
-	_MAKE_MOTATE_PIN(14, J, 1);
-	_MAKE_MOTATE_PIN(15, J, 0);
-	_MAKE_MOTATE_PIN(16, H, 1);
-	_MAKE_MOTATE_PIN(17, H, 0);
-	_MAKE_MOTATE_PIN(18, D, 3);
-	_MAKE_MOTATE_PIN(19, D, 2);
-	_MAKE_MOTATE_PIN(20, D, 1);
-	_MAKE_MOTATE_PIN(21, D, 0);
-	_MAKE_MOTATE_PIN(22, A, 0);
-	_MAKE_MOTATE_PIN(23, A, 1);
-	_MAKE_MOTATE_PIN(24, A, 2);
-	_MAKE_MOTATE_PIN(25, A, 3);
-	_MAKE_MOTATE_PIN(26, A, 4);
-	_MAKE_MOTATE_PIN(27, A, 5);
-	_MAKE_MOTATE_PIN(28, A, 6);
-	_MAKE_MOTATE_PIN(29, A, 7);
-	_MAKE_MOTATE_PIN(30, C, 7);
-	_MAKE_MOTATE_PIN(31, C, 6);
-	_MAKE_MOTATE_PIN(32, C, 5);
-	_MAKE_MOTATE_PIN(33, C, 4);
-	_MAKE_MOTATE_PIN(34, C, 3);
-	_MAKE_MOTATE_PIN(35, C, 2);
-	_MAKE_MOTATE_PIN(36, C, 1);
-	_MAKE_MOTATE_PIN(37, C, 0);
-	_MAKE_MOTATE_PIN(38, D, 7);
-	_MAKE_MOTATE_PIN(39, G, 2);
-	_MAKE_MOTATE_PIN(40, G, 1);
-	_MAKE_MOTATE_PIN(41, G, 0);
-	_MAKE_MOTATE_PIN(42, L, 7);
-	_MAKE_MOTATE_PIN(43, L, 6);
-	_MAKE_MOTATE_PIN(44, L, 5);
-	_MAKE_MOTATE_PIN(45, L, 4);
-	_MAKE_MOTATE_PIN(46, L, 3);
-	_MAKE_MOTATE_PIN(47, L, 2);
-	_MAKE_MOTATE_PIN(48, L, 1);
-	_MAKE_MOTATE_PIN(49, L, 0);
-	_MAKE_MOTATE_PIN(50, B, 3);
-	_MAKE_MOTATE_PIN(51, B, 2);
-	_MAKE_MOTATE_PIN(52, B, 1);
-	_MAKE_MOTATE_PIN(53, B, 0);
-	_MAKE_MOTATE_PIN(54, F, 0);
-	_MAKE_MOTATE_PIN(55, F, 1);
-	_MAKE_MOTATE_PIN(56, F, 2);
-	_MAKE_MOTATE_PIN(57, F, 3);
-	_MAKE_MOTATE_PIN(58, F, 4);
-	_MAKE_MOTATE_PIN(59, F, 5);
-	_MAKE_MOTATE_PIN(60, F, 6);
-	_MAKE_MOTATE_PIN(61, F, 7);
-	_MAKE_MOTATE_PIN(62, K, 0);
-	_MAKE_MOTATE_PIN(63, K, 1);
-	_MAKE_MOTATE_PIN(64, K, 2);
-	_MAKE_MOTATE_PIN(65, K, 3);
-	_MAKE_MOTATE_PIN(66, K, 4);
-	_MAKE_MOTATE_PIN(67, K, 5);
-	_MAKE_MOTATE_PIN(68, K, 6);
-	_MAKE_MOTATE_PIN(69, K, 7);
+	_MAKE_MOTATE_PORT8(A, 'A');
+	_MAKE_MOTATE_PORT8(B, 'B');
+	_MAKE_MOTATE_PORT8(C, 'C');
+	_MAKE_MOTATE_PORT8(D, 'D');
+	_MAKE_MOTATE_PORT8(E, 'E');
+	_MAKE_MOTATE_PORT8(F, 'F');
+	_MAKE_MOTATE_PORT8(G, 'G');
+	_MAKE_MOTATE_PORT8(H, 'H');
+
+	_MAKE_MOTATE_PORT8(J, 'J');
+	_MAKE_MOTATE_PORT8(K, 'K');
+	_MAKE_MOTATE_PORT8(L, 'L');
+
+	_MAKE_MOTATE_PIN(0 , E, 'E', 0);
+	_MAKE_MOTATE_PIN(1 , E, 'E', 1);
+	_MAKE_MOTATE_PIN(2 , E, 'E', 4);
+	_MAKE_MOTATE_PIN(3 , E, 'E', 5);
+	_MAKE_MOTATE_PIN(4 , G, 'G', 5);
+	_MAKE_MOTATE_PIN(5 , E, 'E', 3);
+	_MAKE_MOTATE_PIN(6 , H, 'H', 3);
+	_MAKE_MOTATE_PIN(7 , H, 'H', 4);
+	_MAKE_MOTATE_PIN(8 , H, 'H', 5);
+	_MAKE_MOTATE_PIN(9 , H, 'H', 6);
+	_MAKE_MOTATE_PIN(10, B, 'B', 4);
+	_MAKE_MOTATE_PIN(11, B, 'B', 5);
+	_MAKE_MOTATE_PIN(12, B, 'B', 6);
+	_MAKE_MOTATE_PIN(13, B, 'B', 7);
+	_MAKE_MOTATE_PIN(14, J, 'J', 1);
+	_MAKE_MOTATE_PIN(15, J, 'J', 0);
+	_MAKE_MOTATE_PIN(16, H, 'H', 1);
+	_MAKE_MOTATE_PIN(17, H, 'H', 0);
+	_MAKE_MOTATE_PIN(18, D, 'D', 3);
+	_MAKE_MOTATE_PIN(19, D, 'D', 2);
+	_MAKE_MOTATE_PIN(20, D, 'D', 1);
+	_MAKE_MOTATE_PIN(21, D, 'D', 0);
+	_MAKE_MOTATE_PIN(22, A, 'A', 0);
+	_MAKE_MOTATE_PIN(23, A, 'A', 1);
+	_MAKE_MOTATE_PIN(24, A, 'A', 2);
+	_MAKE_MOTATE_PIN(25, A, 'A', 3);
+	_MAKE_MOTATE_PIN(26, A, 'A', 4);
+	_MAKE_MOTATE_PIN(27, A, 'A', 5);
+	_MAKE_MOTATE_PIN(28, A, 'A', 6);
+	_MAKE_MOTATE_PIN(29, A, 'A', 7);
+	_MAKE_MOTATE_PIN(30, C, 'C', 7);
+	_MAKE_MOTATE_PIN(31, C, 'C', 6);
+	_MAKE_MOTATE_PIN(32, C, 'C', 5);
+	_MAKE_MOTATE_PIN(33, C, 'C', 4);
+	_MAKE_MOTATE_PIN(34, C, 'C', 3);
+	_MAKE_MOTATE_PIN(35, C, 'C', 2);
+	_MAKE_MOTATE_PIN(36, C, 'C', 1);
+	_MAKE_MOTATE_PIN(37, C, 'C', 0);
+	_MAKE_MOTATE_PIN(38, D, 'D', 7);
+	_MAKE_MOTATE_PIN(39, G, 'G', 2);
+	_MAKE_MOTATE_PIN(40, G, 'G', 1);
+	_MAKE_MOTATE_PIN(41, G, 'G', 0);
+	_MAKE_MOTATE_PIN(42, L, 'L', 7);
+	_MAKE_MOTATE_PIN(43, L, 'L', 6);
+	_MAKE_MOTATE_PIN(44, L, 'L', 5);
+	_MAKE_MOTATE_PIN(45, L, 'L', 4);
+	_MAKE_MOTATE_PIN(46, L, 'L', 3);
+	_MAKE_MOTATE_PIN(47, L, 'L', 2);
+	_MAKE_MOTATE_PIN(48, L, 'L', 1);
+	_MAKE_MOTATE_PIN(49, L, 'L', 0);
+	_MAKE_MOTATE_PIN(50, B, 'B', 3);
+	_MAKE_MOTATE_PIN(51, B, 'B', 2);
+	_MAKE_MOTATE_PIN(52, B, 'B', 1);
+	_MAKE_MOTATE_PIN(53, B, 'B', 0);
+	_MAKE_MOTATE_PIN(54, F, 'F', 0);
+	_MAKE_MOTATE_PIN(55, F, 'F', 1);
+	_MAKE_MOTATE_PIN(56, F, 'F', 2);
+	_MAKE_MOTATE_PIN(57, F, 'F', 3);
+	_MAKE_MOTATE_PIN(58, F, 'F', 4);
+	_MAKE_MOTATE_PIN(59, F, 'F', 5);
+	_MAKE_MOTATE_PIN(60, F, 'F', 6);
+	_MAKE_MOTATE_PIN(61, F, 'F', 7);
+	_MAKE_MOTATE_PIN(62, K, 'K', 0);
+	_MAKE_MOTATE_PIN(63, K, 'K', 1);
+	_MAKE_MOTATE_PIN(64, K, 'K', 2);
+	_MAKE_MOTATE_PIN(65, K, 'K', 3);
+	_MAKE_MOTATE_PIN(66, K, 'K', 4);
+	_MAKE_MOTATE_PIN(67, K, 'K', 5);
+	_MAKE_MOTATE_PIN(68, K, 'K', 6);
+	_MAKE_MOTATE_PIN(69, K, 'K', 7);
 	
 #endif
+
+
+	// PinHolder - virtual ports
+	template<uint8_t PinBit7num, uint8_t PinBit6num, uint8_t PinBit5num = -1, uint8_t PinBit4num = -1, uint8_t PinBit3num = -1, uint8_t PinBit2num = -1, uint8_t PinBit1num = -1, uint8_t PinBit0num = -1>
+	class PinHolder {
+
+		static Pin<PinBit7num> PinBit7;
+		static Pin<PinBit6num> PinBit6;
+		static Pin<PinBit5num> PinBit5;
+		static Pin<PinBit4num> PinBit4;
+		static Pin<PinBit3num> PinBit3;
+		static Pin<PinBit2num> PinBit2;
+		static Pin<PinBit1num> PinBit1;
+		static Pin<PinBit0num> PinBit0;
+
+#define _MOTATE_CREATE_CLEAR_AND_COPY_MASKS(aPortLetter) \
+		static const uint8_t port ## aPortLetter ## ClearMask =\
+			(Pin<PinBit7num>::portLetter == Port ## aPortLetter::letter ? Pin<PinBit7num>::mask : 0x00) |\
+			(Pin<PinBit6num>::portLetter == Port ## aPortLetter::letter ? Pin<PinBit6num>::mask : 0x00) |\
+			(Pin<PinBit5num>::portLetter == Port ## aPortLetter::letter ? Pin<PinBit5num>::mask : 0x00) |\
+			(Pin<PinBit4num>::portLetter == Port ## aPortLetter::letter ? Pin<PinBit4num>::mask : 0x00) |\
+			(Pin<PinBit3num>::portLetter == Port ## aPortLetter::letter ? Pin<PinBit3num>::mask : 0x00) |\
+			(Pin<PinBit2num>::portLetter == Port ## aPortLetter::letter ? Pin<PinBit2num>::mask : 0x00) |\
+			(Pin<PinBit1num>::portLetter == Port ## aPortLetter::letter ? Pin<PinBit1num>::mask : 0x00) |\
+			(Pin<PinBit0num>::portLetter == Port ## aPortLetter::letter ? Pin<PinBit0num>::mask : 0x00);\
+\
+		static const uint8_t port ## aPortLetter ## CopyMask =\
+			(Pin<PinBit7num>::portLetter == Port ## aPortLetter::letter && Pin<PinBit7num>::mask == (1 << 7 ) \
+				? Pin<PinBit7num>::mask : 0x00) |\
+			(Pin<PinBit6num>::portLetter == Port ## aPortLetter::letter && Pin<PinBit6num>::mask == (1 << 6 )\
+				? Pin<PinBit6num>::mask : 0x00) |\
+			(Pin<PinBit5num>::portLetter == Port ## aPortLetter::letter && Pin<PinBit5num>::mask == (1 << 5 )\
+				? Pin<PinBit5num>::mask : 0x00) |\
+			(Pin<PinBit4num>::portLetter == Port ## aPortLetter::letter && Pin<PinBit4num>::mask == (1 << 4 )\
+				? Pin<PinBit4num>::mask : 0x00) |\
+			(Pin<PinBit3num>::portLetter == Port ## aPortLetter::letter && Pin<PinBit3num>::mask == (1 << 3 )\
+				? Pin<PinBit3num>::mask : 0x00) |\
+			(Pin<PinBit2num>::portLetter == Port ## aPortLetter::letter && Pin<PinBit2num>::mask == (1 << 2 )\
+				? Pin<PinBit2num>::mask : 0x00) |\
+			(Pin<PinBit1num>::portLetter == Port ## aPortLetter::letter && Pin<PinBit1num>::mask == (1 << 1 )\
+				? Pin<PinBit1num>::mask : 0x00) |\
+			(Pin<PinBit0num>::portLetter == Port ## aPortLetter::letter && Pin<PinBit0num>::mask == (1 << 0 )\
+				? Pin<PinBit0num>::mask : 0x00);
+
+		_MOTATE_CREATE_CLEAR_AND_COPY_MASKS(B);
+		_MOTATE_CREATE_CLEAR_AND_COPY_MASKS(C);
+		_MOTATE_CREATE_CLEAR_AND_COPY_MASKS(D);
+				
+	public:
+		PinHolder() {
+			
+		};
+		
+		void set(uint8_t in_value) {
+			uint8_t port_value    = 0x00; // Port<> handles reading the port and setting the masked pins
+#define _MOTATE_PINHOLDER_CHECKANDSETPIN(portLetter, bitNumber) \
+			if (PinBit ## bitNumber.maskForPort(port ## portLetter.letter) &&\
+					(PinBit ## bitNumber.mask != (1 << bitNumber)) &&\
+					(in_value & (1 << bitNumber))) {\
+				port_value |= PinBit ## bitNumber.mask;\
+			}
+			if (portBClearMask != 0x00) {
+				_MOTATE_PINHOLDER_CHECKANDSETPIN(B, 7);
+				_MOTATE_PINHOLDER_CHECKANDSETPIN(B, 6);
+				_MOTATE_PINHOLDER_CHECKANDSETPIN(B, 5);
+				_MOTATE_PINHOLDER_CHECKANDSETPIN(B, 4);
+				_MOTATE_PINHOLDER_CHECKANDSETPIN(B, 3);
+				_MOTATE_PINHOLDER_CHECKANDSETPIN(B, 2);
+				_MOTATE_PINHOLDER_CHECKANDSETPIN(B, 1);
+				_MOTATE_PINHOLDER_CHECKANDSETPIN(B, 0);
+				port_value |= in_value & ~portBCopyMask;
+				portB.setPins(port_value, portBClearMask);
+			}
+
+			if (portCClearMask != 0x00) {
+				port_value    = 0x00; // Port<> handles reading the port and setting the masked pins
+				_MOTATE_PINHOLDER_CHECKANDSETPIN(C, 7);
+				_MOTATE_PINHOLDER_CHECKANDSETPIN(C, 6);
+				_MOTATE_PINHOLDER_CHECKANDSETPIN(C, 5);
+				_MOTATE_PINHOLDER_CHECKANDSETPIN(C, 4);
+				_MOTATE_PINHOLDER_CHECKANDSETPIN(C, 3);
+				_MOTATE_PINHOLDER_CHECKANDSETPIN(C, 2);
+				_MOTATE_PINHOLDER_CHECKANDSETPIN(C, 1);
+				_MOTATE_PINHOLDER_CHECKANDSETPIN(C, 0);
+				port_value |= in_value & ~portCCopyMask;
+				portC.setPins(port_value, portCClearMask);
+			}
+			
+			if (portDClearMask != 0x00) {
+				port_value    = 0x00; // Port<> handles reading the port and setting the masked pins
+				_MOTATE_PINHOLDER_CHECKANDSETPIN(D, 7);
+				_MOTATE_PINHOLDER_CHECKANDSETPIN(D, 6);
+				_MOTATE_PINHOLDER_CHECKANDSETPIN(D, 5);
+				_MOTATE_PINHOLDER_CHECKANDSETPIN(D, 4);
+				_MOTATE_PINHOLDER_CHECKANDSETPIN(D, 3);
+				_MOTATE_PINHOLDER_CHECKANDSETPIN(D, 2);
+				_MOTATE_PINHOLDER_CHECKANDSETPIN(D, 1);
+				_MOTATE_PINHOLDER_CHECKANDSETPIN(D, 0);
+				port_value |= in_value & ~portCCopyMask;
+				portD.setPins(port_value, portDClearMask);
+			}
+		}
+		
+	};
 
 }
 
--- README.md
+++ README.md
@@ -1,4 +1,121 @@
-Motate
-======
+# Motate
 
 Motate: An Arduino-compatible board and software designed with movement in mind.
+
+# Status
+
+Currently the hardware is still in prototyping, but as the software components are closer to ready I'll release them here for review.
+
+**The interface for this code is still in flux.** Before the Motate system is completed I'll freeze the interface.
+
+Please share any suggestions for changes. Pull requests welcome as well.
+
+# Usage
+
+The contents of the Arduino Library folder can be placed the appropriate arduino libraries folder for your system, and the arduino IDE reloaded. The examples will show up in your File-> Examples submenu.
+
+# Libraries
+
+##Motate::Pin
+
+At the top of the file, you need to pull in the header:
+
+	#include <MotatePins.h>
+	// save typing Motate::Pin
+	using namespace Motate; 
+
+### Creating `Pin`s
+Creating a `Pin` (uppercase P) object is easy. You can use the pre-defined types `Pin`X or the template syntax `Pin<`X`>`.
+
+	// create the led Pin object, setup as an output
+	Pin13 ledPin = Output;
+	// OR
+	Pin<13> is acceptable as well.
+
+Also as a convenience you can use the predefined objects `pin`N (lowercase p).
+
+	pin13 = Output;
+	pin13 = HIGH;
+	if (pin13) {
+		// something
+	}
+
+You generally cannot use `Pin<variableName>` since `variableName` cannot be changed after compiling.
+	
+However, `const uint8_t` variables can be used as the pin number, since they cannot be changed and must be set when they're defined.
+
+The typedef `pin_number` was created as a more readable shortcut, and can be used for consolidating configuration of pins, for example.
+
+	pin_number led_num = 13;
+	// ... later ...
+	Pin<led_num> ledPin = Output;
+
+### Configuring `Pin`s
+
+Setting the pin direction is easy. You assign one of the special values `Input`, `InputWithPullup`, and `Output` to the pin at any time.
+
+	ledPin = Output;
+	Pin3 inputPin = Input;
+	Pin4 input2 = InputWithPullup;
+
+The `InputWithPullup` is a convenience value for `Input` followed by setting the pin to `HIGH`.
+
+### Setting `Pin`s
+
+Setting the pin value is easy:
+
+	ledPin = HIGH;
+	// OR
+	ledPin = 1;
+	
+	// Non-zero is HIGH, so you can use the result of bit-math:
+	ledPin = input_value & test_mask;
+	
+	// Warning, negative is high too!
+	int bad_value = -1;
+	ledPin = badValue; // <- turns the led ON
+
+**Note:** Setting the pin *value* is not supported while creating the pin.
+
+This is **not** valid:
+
+	Pin3 inputPin = LOW; // <-- NOT VALID
+
+### Reading `Pin`s
+
+Reading the pin is just as easy:
+
+	Pin3 inputPin = InputWithPullup;
+	if (inputPin) {
+		// it's HIGH
+	} else {
+		// it's LOW
+	}
+
+### Warnings
+
+A Pin is a pin, so every Pin that refers to the same physical pin will have the same attributes, since they are nothing but wrappers to manipulate the internal registers.
+
+	// Set pin 5 to an input
+	pin5 = Input; 
+	
+	// Create a new pin 5 and make in an output
+	Pin<5> otherPin = Output;
+	
+`pin5` and `otherPin` are both Outputs at the end.
+
+Also, `Pin` objects don't set their `Input`/`Output` state automatically. A `Pin` set to `Input` and set to `HIGH` will turn on the pull-up. A `Pin` set to `Output` then read will simply return the value it was last set to.
+
+### Interaction with `digitalWrite()` and `digitalRead()`
+
+`digitalRead()` and `digitalWrite()` are completely compatible with Pin changes. They work differently, but are both changing the same internal pin registers.
+
+### Analog values
+
+`Motate::Pin` currently does not **yet** support analog pin features. Of particular note is that an `analogWrite()` on a pin that is then set with a `Pin` object will not change the PWM ("analog") output value.
+
+##Motate::LiquidCrystal
+
+More info to come.
+
+-Rob
