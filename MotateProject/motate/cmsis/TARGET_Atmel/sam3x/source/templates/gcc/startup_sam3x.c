4c91fc764ca185b3045e14c5caf916b22183a78c
Rob Giseburt <giseburt@gmail.com>
2019-10-16 22:33:37 -0500

    Fixing sam3x startup

--- MotateProject/motate/cmsis/TARGET_Atmel/sam3x/source/templates/gcc/startup_sam3x.c
+++ MotateProject/motate/cmsis/TARGET_Atmel/sam3x/source/templates/gcc/startup_sam3x.c
@@ -60,10 +60,10 @@ extern uint32_t _sstack;
 extern uint32_t _estack;
 
 /** \cond DOXYGEN_SHOULD_SKIP_THIS */
-int main(void);
+extern int main(void);
 /** \endcond */
 
-void __libc_init_array(void);
+extern void __libc_init_array(void);
 
 /* Exception Table */
 __attribute__ ((section(".vectors")))
@@ -71,111 +71,111 @@ IntFunc exception_table[] = {
 
 	/* Configure Initial Stack Pointer, using linker-generated symbols */
 	(IntFunc) (&_estack),
-	(void*) Reset_Handler,
+	Reset_Handler,
 
-	(void*) NMI_Handler,
-	(void*) HardFault_Handler,
-	(void*) MemManage_Handler,
-	(void*) BusFault_Handler,
-	(void*) UsageFault_Handler,
-	(void*) (0UL),           /* Reserved */
-	(void*) (0UL),           /* Reserved */
-	(void*) (0UL),           /* Reserved */
-	(void*) (0UL),           /* Reserved */
-	(void*) SVC_Handler,
-	(void*) DebugMon_Handler,
-	(void*) (0UL),           /* Reserved */
-	(void*) PendSV_Handler,
-	(void*) SysTick_Handler,
+	NMI_Handler,
+	HardFault_Handler,
+	MemManage_Handler,
+	BusFault_Handler,
+	UsageFault_Handler,
+	(0UL),           /* Reserved */
+	(0UL),           /* Reserved */
+	(0UL),           /* Reserved */
+	(0UL),           /* Reserved */
+	SVC_Handler,
+	DebugMon_Handler,
+	(0UL),           /* Reserved */
+	PendSV_Handler,
+	SysTick_Handler,
 
 	/* Configurable interrupts */
-	(void*) SUPC_Handler,    /*  0  Supply Controller */
-	(void*) RSTC_Handler,    /*  1  Reset Controller */
-	(void*) RTC_Handler,     /*  2  Real Time Clock */
-	(void*) RTT_Handler,     /*  3  Real Time Timer */
-	(void*) WDT_Handler,     /*  4  Watchdog Timer */
-	(void*) PMC_Handler,     /*  5  PMC */
-	(void*) EFC0_Handler,    /*  6  EFC 0 */
-	(void*) EFC1_Handler,    /*  7  EFC 1 */
-	(void*) UART_Handler,    /*  8  UART */
+	SUPC_Handler,    /*  0  Supply Controller */
+	RSTC_Handler,    /*  1  Reset Controller */
+	RTC_Handler,     /*  2  Real Time Clock */
+	RTT_Handler,     /*  3  Real Time Timer */
+	WDT_Handler,     /*  4  Watchdog Timer */
+	PMC_Handler,     /*  5  PMC */
+	EFC0_Handler,    /*  6  EFC 0 */
+	EFC1_Handler,    /*  7  EFC 1 */
+	UART_Handler,    /*  8  UART */
 #ifdef _SAM3XA_SMC_INSTANCE_
-	(void*) SMC_Handler,     /*  9  SMC */
+	SMC_Handler,     /*  9  SMC */
 #else
-	(void*) (0UL),           /*  9 Reserved */
+	(0UL),           /*  9 Reserved */
 #endif /* _SAM3XA_SMC_INSTANCE_ */
 #ifdef _SAM3XA_SDRAMC_INSTANCE_
-	(void*) SDRAMC_Handler,  /* 10  SDRAMC */
+	SDRAMC_Handler,  /* 10  SDRAMC */
 #else
-	(void*) (0UL),           /* 10 Reserved */
+	(0UL),           /* 10 Reserved */
 #endif /* _SAM3XA_SDRAMC_INSTANCE_ */
-	(void*) PIOA_Handler,    /* 11 Parallel IO Controller A */
-	(void*) PIOB_Handler,    /* 12 Parallel IO Controller B */
+	PIOA_Handler,    /* 11 Parallel IO Controller A */
+	PIOB_Handler,    /* 12 Parallel IO Controller B */
 #ifdef _SAM3XA_PIOC_INSTANCE_
-	(void*) PIOC_Handler,    /* 13 Parallel IO Controller C */
+	PIOC_Handler,    /* 13 Parallel IO Controller C */
 #else
-	(void*) (0UL),           /* 13 Reserved */
+	(0UL),           /* 13 Reserved */
 #endif /* _SAM3XA_PIOC_INSTANCE_ */
 #ifdef _SAM3XA_PIOD_INSTANCE_
-	(void*) PIOD_Handler,    /* 14 Parallel IO Controller D */
+	PIOD_Handler,    /* 14 Parallel IO Controller D */
 #else
-	(void*) (0UL),           /* 14 Reserved */
+	(0UL),           /* 14 Reserved */
 #endif /* _SAM3XA_PIOD_INSTANCE_ */
 #ifdef _SAM3XA_PIOE_INSTANCE_
-	(void*) PIOE_Handler,    /* 15 Parallel IO Controller E */
+	PIOE_Handler,    /* 15 Parallel IO Controller E */
 #else
-	(void*) (0UL),           /* 15 Reserved */
+	(0UL),           /* 15 Reserved */
 #endif /* _SAM3XA_PIOE_INSTANCE_ */
 #ifdef _SAM3XA_PIOF_INSTANCE_
-	(void*) PIOF_Handler,    /* 16 Parallel IO Controller F */
+	PIOF_Handler,    /* 16 Parallel IO Controller F */
 #else
-	(void*) (0UL),           /* 16 Reserved */
+	(0UL),           /* 16 Reserved */
 #endif /* _SAM3XA_PIOF_INSTANCE_ */
-	(void*) USART0_Handler,  /* 17 USART 0 */
-	(void*) USART1_Handler,  /* 18 USART 1 */
-	(void*) USART2_Handler,  /* 19 USART 2 */
+	USART0_Handler,  /* 17 USART 0 */
+	USART1_Handler,  /* 18 USART 1 */
+	USART2_Handler,  /* 19 USART 2 */
 #ifdef _SAM3XA_USART3_INSTANCE_
-	(void*) USART3_Handler,  /* 20 USART 3 */
+	USART3_Handler,  /* 20 USART 3 */
 #else
-	(void*) (0UL),           /* 20 Reserved */
+	(0UL),           /* 20 Reserved */
 #endif /* _SAM3XA_USART3_INSTANCE_ */
-	(void*) HSMCI_Handler,   /* 21 MCI */
-	(void*) TWI0_Handler,    /* 22 TWI 0 */
-	(void*) TWI1_Handler,    /* 23 TWI 1 */
-	(void*) SPI0_Handler,    /* 24 SPI 0 */
+	HSMCI_Handler,   /* 21 MCI */
+	TWI0_Handler,    /* 22 TWI 0 */
+	TWI1_Handler,    /* 23 TWI 1 */
+	SPI0_Handler,    /* 24 SPI 0 */
 #ifdef _SAM3XA_SPI1_INSTANCE_
-	(void*) SPI1_Handler,    /* 25 SPI 1 */
+	SPI1_Handler,    /* 25 SPI 1 */
 #else
-	(void*) (0UL),           /* 25 Reserved */
+	(0UL),           /* 25 Reserved */
 #endif /* _SAM3XA_SPI1_INSTANCE_ */
-	(void*) SSC_Handler,     /* 26 SSC */
-	(void*) TC0_Handler,     /* 27 Timer Counter 0 */
-	(void*) TC1_Handler,     /* 28 Timer Counter 1 */
-	(void*) TC2_Handler,     /* 29 Timer Counter 2 */
-	(void*) TC3_Handler,     /* 30 Timer Counter 3 */
-	(void*) TC4_Handler,     /* 31 Timer Counter 4 */
-	(void*) TC5_Handler,     /* 32 Timer Counter 5 */
+	SSC_Handler,     /* 26 SSC */
+	TC0_Handler,     /* 27 Timer Counter 0 */
+	TC1_Handler,     /* 28 Timer Counter 1 */
+	TC2_Handler,     /* 29 Timer Counter 2 */
+	TC3_Handler,     /* 30 Timer Counter 3 */
+	TC4_Handler,     /* 31 Timer Counter 4 */
+	TC5_Handler,     /* 32 Timer Counter 5 */
 #ifdef _SAM3XA_TC2_INSTANCE_
-	(void*) TC6_Handler,     /* 33 Timer Counter 6 */
-	(void*) TC7_Handler,     /* 34 Timer Counter 7 */
-	(void*) TC8_Handler,     /* 35 Timer Counter 8 */
+	TC6_Handler,     /* 33 Timer Counter 6 */
+	TC7_Handler,     /* 34 Timer Counter 7 */
+	TC8_Handler,     /* 35 Timer Counter 8 */
 #else
-	(void*) (0UL),           /* 33 Reserved */
-	(void*) (0UL),           /* 34 Reserved */
-	(void*) (0UL),           /* 35 Reserved */
+	(0UL),           /* 33 Reserved */
+	(0UL),           /* 34 Reserved */
+	(0UL),           /* 35 Reserved */
 #endif /* _SAM3XA_TC2_INSTANCE_ */
-	(void*) PWM_Handler,     /* 36 PWM */
-	(void*) ADC_Handler,     /* 37 ADC controller */
-	(void*) DACC_Handler,    /* 38 DAC controller */
-	(void*) DMAC_Handler,    /* 39 DMA Controller */
-	(void*) UOTGHS_Handler,  /* 40 USB OTG High Speed */
-	(void*) TRNG_Handler,    /* 41 True Random Number Generator */
+	PWM_Handler,     /* 36 PWM */
+	ADC_Handler,     /* 37 ADC controller */
+	DACC_Handler,    /* 38 DAC controller */
+	DMAC_Handler,    /* 39 DMA Controller */
+	UOTGHS_Handler,  /* 40 USB OTG High Speed */
+	TRNG_Handler,    /* 41 True Random Number Generator */
 #ifdef _SAM3XA_EMAC_INSTANCE_
-	(void*) EMAC_Handler,    /* 42 Ethernet MAC */
+	EMAC_Handler,    /* 42 Ethernet MAC */
 #else
-	(void*) (0UL),           /* 42 Reserved */
+	(0UL),           /* 42 Reserved */
 #endif /* _SAM3XA_EMAC_INSTANCE_ */
-	(void*) CAN0_Handler,    /* 43 CAN Controller 0 */
-	(void*) CAN1_Handler     /* 44 CAN Controller 1 */
+	CAN0_Handler,    /* 43 CAN Controller 0 */
+	CAN1_Handler     /* 44 CAN Controller 1 */
 };
 
 /* TEMPORARY PATCH FOR SCB */
